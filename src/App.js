9cb5d5cf62c0890b872fbe8ed2084666e7aa1868
Nolwenn 22 <lotier77@gmail.com>
2021-06-04 14:17:51 +0200

    Local Storage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     super();
     this.state = {
       products : data.products,
-      cartItems : [],
+      cartItems : localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: "",
     };
@@ -18,7 +18,8 @@ class App extends React.Component {
     const cartItems = this.state.cartItems.slice();
     this.setState({
       cartItems : cartItems.filter(x => x._id !== product._id),
-    })
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x => x._id !== product._id)));
   }
 
   addToCart = product => {
@@ -34,6 +35,7 @@ class App extends React.Component {
       cartItems.push({...product, count : 1});
     }
     this.setState({cartItems});
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
   sortProducts = (event) => {
